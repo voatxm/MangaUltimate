068a25f51715d23b5aeb2f4656bd491733076e03
justatest420 <justatestsubject.01@gmail.com>
2024-12-05 05:10:36 +0545

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ async def async_main():
 if __name__ == '__main__':
     loop = aio.get_event_loop_policy().get_event_loop()
     loop.run_until_complete(async_main())
-    #loop.create_task(manga_updater())
-    #for i in range(10):
-        #loop.create_task(chapter_creation(i + 1))
+    loop.create_task(manga_updater())
+    for i in range(10):
+        loop.create_task(chapter_creation(i + 1))
     bot.run()
