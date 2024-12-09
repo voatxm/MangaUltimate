4d7109ea7d218da95dee6973b7482d3bf93baf09
VØAT <71224541+voatxm@users.noreply.github.com>
2024-12-09 09:39:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,20 +10,29 @@ env_vars = {
   # Get For tembo.io
   "DATABASE_URL_PRIMARY": "postgresql://postgres:4NQDGksWwMhyfcP5@delicately-purposeful-hen.data-1.apse1.tembo.io:5432/postgres",
   # Logs Channel Username Without @
-  "CACHE_CHANNEL": "Dump2075",
+  "CACHE_CHANNEL": "",
   # Force Subs Channel username without @
-  "CHANNEL": "Manga_Campus",
+  "CHANNEL": "",
   # {chap_num}: Chapter Number
   # {chap_name} : Manga Name
   # Ex : Chapter {chap_num} {chap_name} @Manhwa_Arena
   "F1": "[MC] [{chap_num}] {chap_name} @Manga_Campus",
   "F2": "[{chap_num}] [MW] {chap_name} [@Manhwa_Weebs]",
+  "F3": "[MY] [{chap_num}] {chap_name} @Manga_Yugen",
+  "F4": "[MM] [Ch-{chap_num}] {chap_name} @Manga_Mayhem",
+  "F5": "[M18] [{chap_num}] {chap_name} @Manhwa18cc",
 # Thumb 
   "TH1": "TH1.jpg",
   "TH2": "TH2.jpg",
+  "TH3": "TH3.jpg",
+  "TH4": "TH4.jpg",
+  "TH5": "TH5.jpg",
   #Banner
   "B1": ["first.jpg", "last.jpg"],
-  "B2": ["TH2.jpg", "thumb.jpg"]
+  "B2": ["TH2.jpg", "thumb.jpg"],
+  "B3": ["my.jpg", "my.jpg"],
+  "B4": ["TH4.jpg", "TH4.jpg"],
+  "B5": ["m18.jpg", "m18.jpg"]
 }
 #OWNER_ID = int(os.environ.get("OWNER_ID", "1788144071")) # Retrieve the AUTH_USERS environment variable as a space-separated string and convert to a list of integers 
 #auth_users = [int(user_id) for user_id in os.environ.get('5164955785,7716045686,6975428639,1302933634').split()] # Append OWNER_ID to the list of auth_users 
