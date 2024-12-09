5dd4815b02ad326dadf3252402e8504766f8fdbd
VØAT <71224541+voatxm@users.noreply.github.com>
2024-12-09 12:03:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ env_vars = {
   # Get From my.telegram.org
   "API_ID": "20445873",
   #Get For @BotFather
-  "BOT_TOKEN": "8034468565:AAGm2i6LM3FJ6WnoF6D2joZwc2cw6ZIiGAU",
+  "BOT_TOKEN": "7142918767:AAEEcdJ5WuMTtwctzB0PHha44mVIn7FZVBs",
   # Get For tembo.io
   "DATABASE_URL_PRIMARY": "postgresql://postgres:4NQDGksWwMhyfcP5@delicately-purposeful-hen.data-1.apse1.tembo.io:5432/postgres",
   # Logs Channel Username Without @
